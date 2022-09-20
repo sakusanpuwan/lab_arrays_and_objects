aa946161a6d36a0000c17b4487c6f65f2aa765e6
sakusanpuwan <sakusanpuwan98@gmail.com>
2022-09-20 16:28:37 +0100

    added extensions

--- advanced.js
+++ advanced.js
@@ -42,19 +42,21 @@ unitedKingdom = [
  * Q1. Add some tourist attractions to England
  */
 
-england = null;
-
-// console.log(england);
+englandIndex = unitedKingdom.findIndex((value) => value.name === "England");
+unitedKingdom[englandIndex].touristAttractions = ["Big Ben","Buckingham Palace","Stone Henge"];
+england = unitedKingdom[englandIndex];
+//console.log(england);
 
 
 
 /**
  * Q2. Change the capital of Wales to "Cardiff"
  */
+walesIndex = unitedKingdom.findIndex((value) => value.name === "Wales");
+unitedKingdom[walesIndex].capital = "Cardiff";
+wales = unitedKingdom[walesIndex];
 
-wales = null;
-
-// console.log(wales);
+//console.log(wales);
 
 
 
@@ -62,12 +64,26 @@ wales = null;
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
 
-northernIrelandKeys = null;
-
-// console.log(northernIrelandKeys);
+northernIrelandIndex = unitedKingdom.findIndex((value) => value.name === "Northern Ireland");
+northernIrelandKeys = Object.keys(unitedKingdom[northernIrelandIndex]);
+//console.log(northernIrelandKeys);
 
 
 
 /**
  * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, smallest or somewhere in the middle.
  */
+
+scotlandIndex = unitedKingdom.findIndex((value) => value.name === "Scotland");
+scotlandPopulation = unitedKingdom[scotlandIndex].population;
+countriesByPopulation = [unitedKingdom[0]];
+
+for (let index = 1; index < unitedKingdom.length; index++) {
+    const country = unitedKingdom[index];
+    if (country.population > countriesByPopulation[index - 1].population) {
+        countriesByPopulation.splice(index - 1,0,country);
+    }
+
+}
+
+console.log(countriesByPopulation);
